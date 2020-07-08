8175a09cf93c5d6cd2b6308aeff64a62b9a6e1bd
mobbex <admin@mobbex.com>
2020-07-08 10:24:19 -0300

    Upgrading embed to version 1.0.8

--- utils.php
+++ utils.php
@@ -7,8 +7,8 @@ define('MOBBEX_CHECKOUT', 'https://api.mobbex.com/p/checkout');
 define('MOBBEX_WC_GATEWAY', 'WC_Gateway_Mobbex');
 define('MOBBEX_WC_GATEWAY_ID', 'mobbex');
 
-define('MOBBEX_VERSION', '2.3.6');
-define('MOBBEX_EMBED_VERSION', '1.0.6');
+define('MOBBEX_VERSION', '2.3.7');
+define('MOBBEX_EMBED_VERSION', '1.0.8');
 
 if (!function_exists('mobbex_debug')) {
     // https://github.com/bonny/WordPress-Simple-History/
