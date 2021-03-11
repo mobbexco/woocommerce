39a0320e1bfa0d800039ac9df218eb8eb62e5d7c
jlmobbex <javier.lopez@mobbex.com>
2021-03-11 18:46:32 -0300

    Update version

--- mobbex-for-woocommerce.php
+++ mobbex-for-woocommerce.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:  Mobbex for Woocommerce
 Description:  A small plugin that provides Woocommerce <-> Mobbex integration.
-Version:      3.1.5
+Version:      3.1.6
 WC tested up to: 4.6.1
 Author: mobbex.com
 Author URI: https://mobbex.com/
