a24ab2a23fd05385f26109aa20674f258d840c8e
jlmobbex <javier.lopez@mobbex.com>
2021-04-19 10:13:57 -0300

    Set default empty options in helper

--- includes/helper.php
+++ includes/helper.php
@@ -8,7 +8,7 @@ class MobbexHelper
     {
         // Init settings (Full List in WC_Gateway_Mobbex::init_form_fields)
         $option_key = 'woocommerce_' . MOBBEX_WC_GATEWAY_ID . '_settings';
-		$settings = get_option($option_key, null);
+        $settings = get_option($option_key, null) ?: [];
         foreach ($settings as $key => $value) {
             $key = str_replace('-', '_', $key);
             $this->$key = $value;
