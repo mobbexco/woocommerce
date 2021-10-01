360a776cf0f57ba26c2f5efc753d218a966c5316
jlmobbex <javier.lopez@mobbex.com>
2021-10-01 18:13:00 -0300

    New order helper class

---
+++ includes/helper/class-order-helper.php
@@ -0,0 +1,136 @@
+<?php
+
+class MobbexOrderHelper
+{
+    /** Order instance ID */
+    public $id;
+
+    /** @var WC_Order */
+    public $order;
+
+    /** @var MobbexHelper */
+    public $helper;
+
+    /**
+    * Constructor.
+    * 
+    * @param WC_Order WooCommerce Order instance.
+    * @param MobbexHelper Base plugin helper.
+    */
+    public function __construct($order, $helper = null)
+    {
+        $this->id     = $order->get_id();
+        $this->order  = $order;
+        $this->helper = $helper ?: new MobbexHelper();
+    }
+
+    /**
+     * Create a checkout using the WooCommerce Order instance.
+     * 
+     * @return mixed
+     */
+    public function create_checkout()
+    {
+        // Try to configure api with order store credentials
+        $this->helper->get_store($this->order);
+
+        $api_key      = !empty($store['api_key']) ? $store['api_key'] : $this->helper->settings['api-key'];
+        $access_token = !empty($store['access_token']) ? $store['access_token'] : $this->helper->settings['access-token'];
+
+        $checkout = new MobbexCheckout($this->helper->settings, new MobbexApi($api_key, $access_token));
+
+        $this->add_initial_data($checkout);
+        $this->add_items($checkout);
+        $this->add_installments($checkout);
+        $this->add_customer($checkout);
+
+        try {
+            $response = $checkout->create();
+        } catch (\Exception $e) {
+            $response = null;
+            $this->helper->debug('Mobbex Checkout Creation Failed: ' . $e->getMessage(), isset($e->data) ? $e->data : ''); // TODO: Add new debug method
+        }
+
+        do_action('mobbex_checkout_process', $response, $this->id);
+
+        return $response;
+    }
+
+    /**
+     * Add order initial data to checkout.
+     * 
+     * @param MobbexCheckout $checkout
+     */
+    private function add_initial_data($checkout)
+    {
+        $checkout->set_reference($this->id);
+        $checkout->set_total($this->order->get_total());
+        $checkout->set_endpoints(
+            $this->helper->get_api_endpoint('mobbex_return_url', $this->id),
+            $this->helper->get_api_endpoint('mobbex_webhook', $this->id),
+        );
+    }
+
+    /**
+     * Add order items to checkout.
+     * 
+     * @param MobbexCheckout $checkout
+     */
+    private function add_items($checkout)
+    {
+        $order_items    = $this->order->get_items() ?: [];
+        $shipping_items = $this->order->get_items('shipping') ?: [];
+
+        foreach ($order_items as $item)
+            $checkout->add_item($item->get_total(), $item->get_quantity(), $item->get_name(), $this->helper->get_product_image($item->get_product_id()));
+
+        foreach ($shipping_items as $item)
+            $checkout->add_item($item->get_total(), 1, __('Shipping: ', 'mobbex-for-woocommerce') . $item->get_name());
+    }
+
+    /**
+     * Add installments configured to checkout.
+     * 
+     * @param MobbexCheckout $checkout
+     */
+    private function add_installments($checkout)
+    {
+        $inactive_plans = $active_plans = [];
+        $products = $this->helper::get_product_ids($this->order);
+
+        // Get plans from order products
+        foreach ($products as $product_id) {
+            $inactive_plans = array_merge($inactive_plans, $this->helper::get_inactive_plans($product_id));
+            $active_plans   = array_merge($active_plans, $this->helper::get_active_plans($product_id));
+        }
+
+        // Block inactive (common) plans from installments
+        foreach ($inactive_plans as $plan_ref)
+            $checkout->block_installment($plan_ref);
+
+        // Add active (advanced) plans to installments (only if the plan is active on all products)
+        foreach (array_count_values($active_plans) as $plan_uid => $reps) {
+            if ($reps == count($products))
+                $checkout->add_installment($plan_uid);
+        }
+    }
+
+    /**
+     * Add order customer data to checkout.
+     * 
+     * @param MobbexCheckout $checkout
+     */
+    private function add_customer($checkout)
+    {
+        $user    = wp_get_current_user();
+        $dni_key = !empty($this->helper->settings['custom_dni']) ? $this->helper->settings['custom_dni'] : '_billing_dni';
+
+        $checkout->set_customer(
+            $this->order->get_formatted_billing_full_name() ?: $user->display_name,
+            $this->order->get_billing_email() ?: $user->user_email,
+            get_post_meta($this->id, $dni_key, true),
+            $this->order->get_billing_phone() ?: get_user_meta($user->ID, 'phone_number', true),
+            $user->ID,
+        );
+    }
+}
