8fcf1d581d2a91e44875be5ea9f6b92158219dd2
jlmobbex <javier.lopez@mobbex.com>
2022-01-24 19:42:24 -0300

    Fix headers obtaining

--- includes/helper.php
+++ includes/helper.php
@@ -574,11 +574,11 @@ class MobbexHelper
     public function get_headers()
     {
         return [
-            'cache-control: no-cache',
-            'content-type: application/json',
-            'x-api-key: ' . $this->api_key,
-            'x-access-token: ' . $this->access_token,
-            'x-ecommerce-agent: WordPress/' . get_bloginfo('version') . ' WooCommerce/' . WC_VERSION . ' Plugin/' . MOBBEX_VERSION,
+            'cache-control'     => 'no-cache',
+            'content-type'      => 'application/json',
+            'x-api-key'         => $this->api_key,
+            'x-access-token'    => $this->access_token,
+            'x-ecommerce-agent' => 'WordPress/' . get_bloginfo('version') . ' WooCommerce/' . WC_VERSION . ' Plugin/' . MOBBEX_VERSION,
         ];
     }
 }
