83982791f78041b7ed0f455085d977712ae9f20e
jlmobbex <javier.lopez@mobbex.com>
2022-07-07 00:14:49 -0300

    Fix support for php 7 installs

--- includes/helper/class-order-helper.php
+++ includes/helper/class-order-helper.php
@@ -94,7 +94,7 @@ class MobbexOrderHelper
                 $item->get_name(),
                 $this->helper->get_product_image($item->get_product_id()),
                 $this->helper->get_product_entity($item->get_product_id()),
-                $this->helper->get_product_subscription($item->get_product_id()),
+                $this->helper->get_product_subscription($item->get_product_id())
             );
 
         foreach ($shipping_items as $item)
