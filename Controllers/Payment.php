f5e5de47bddb4f1d5bd3d35b4dc0b1e2108d7526
EMunozMobbex <e.munoz.chedel@mobbex.com>
2023-03-16 17:37:32 -0300

    fix: fixed syntax errors in payment controller

---
+++ Controllers/Payment.php
@@ -0,0 +1,8 @@
+            $this->logger->log("REST API > Error", $e->getMessage());
+        $this->logger->log('Mobbex Webhook: Processing data');
+            return $this->logger->log('Mobbex Webhook: Invalid mobbex token or empty data');
+        if (isset($data['risk_analysis']) && $data['risk_analysis'] > 0) {
+            $this->logger->log("REST API > Error", $e->getMessage());
+        $this->logger->log('Mobbex Webhook: Processing data');
+            return $this->logger->log('Mobbex Webhook: Invalid mobbex token or empty data');
+        if (isset($data['risk_analysis']) && $data['risk_analysis'] > 0) {
