a6be593d9c50cdbfd29e2a17438432c7d919aaf5
mobbexLuis <luis.scarrone@mobbex.com>
2023-08-29 15:48:47 -0300

    add new methods to insert and select sata in db

--- Model/Db.php
+++ Model/Db.php
@@ -38,4 +38,46 @@ class Db extends \Mobbex\Model\Db
             return $this->db->get_results($sql, ARRAY_A);
 
     }
+
+    /**
+     * Insert data in specific table. Columns and values can be passed
+     * 
+     * @param string $table_name
+     * @param array  $columns optional it can contain column name and its value as assosiative array
+     * 
+     */
+    public function insert($table_name, $columns = [])
+    {
+        $this->db->insert($this->prefix . $table_name, $columns);
+    }
+
+    /**
+     * Select data from specifict table. Filters, limit and offset can be passed
+     * 
+     * @param string $table_name
+     * @param array  $filters
+     * @param string|int  $limit
+     * @param string|int  $offset
+     * 
+     * @return array table data
+     * 
+     */
+    public function select($table_name, $filters = [], $limit = '', $offset = '' )
+    {
+        // Check if there is more than one filter to build the query correctly
+        if (count(array_filter($filters, null)) > 1){
+            $filters = array_filter($filters, null);
+            $filters = implode(' AND ', $filters);
+        }
+        else {
+            $filters = implode('', $filters);
+        }
+
+        // Sets query restriction clauses
+        $limit      = !empty($limit) ? "LIMIT $limit" : '' ;
+        $offset     = !empty($offset) ? "OFFSET $offset" : '' ;
+        $conditions = !empty($filters) ? "WHERE $filters" : '' ;
+        
+        return $this->db->get_results("SELECT * FROM  $this->prefix$table_name $conditions $limit $offset;" , ARRAY_A);
+    }
 }
