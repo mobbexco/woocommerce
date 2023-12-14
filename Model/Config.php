0a9304016845be07d8d79477e3df427d27e67efe
EMunozMobbex <e.munoz.chedel@mobbex.com>
2023-12-14 11:55:00 -0300

    refactor: Added new format checks to obtaining mobbex plans, to ensure that they are returned as an array

--- Model/Config.php
+++ Model/Config.php
@@ -88,10 +88,31 @@ class Config
      */
     public function get_catalog_settings($id, $field_name, $catalog_type = 'post')
     {
-        if (strpos($field_name, '_plans'))
-            return get_metadata($catalog_type, $id, $field_name, true) ?: [];
+        $data = get_metadata($catalog_type, $id, $field_name, true);
 
-        return get_metadata($catalog_type, $id, $field_name, true) ?: '';
+        if (strpos($field_name, '_plans'))
+            return $data ? $this->maybe_decode($data) : [];
+
+        return $data ?: '';
+    }
+
+    /**
+     * This method checks the format of the metadata & transforms it into an array if necessary.
+     * 
+     * @param string|array $plans
+     * 
+     * @return array|null
+     */
+    private function maybe_decode($plans)
+    {
+        if(is_array($plans))
+            return $plans;
+
+        if (is_serialized($plans))
+            return unserialize($plans);
+        
+        if (json_decode($plans, true))
+            return json_decode($plans, true);
     }
 
     /**
@@ -173,7 +194,6 @@ class Config
 
        return compact('common_plans', 'advanced_plans');
     }
-
 
     /**
      * Get current store data from product or product category.
