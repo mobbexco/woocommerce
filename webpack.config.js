d5a39ef312037c04b13f92e55afc20a82ba37547
mobbexLuis <luis.scarrone@mobbex.com>
2026-01-23 17:48:38 -0300

    updated webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,54 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
-const path = require('path');
+const defaultConfig = require("@wordpress/scripts/config/webpack.config.js");
+const WooCommerceDependencyExtractionWebpackPlugin = require("@woocommerce/dependency-extraction-webpack-plugin");
+const path = require("path");
 
 const wcDepMap = {
-	'@woocommerce/blocks-registry': ['wc', 'wcBlocksRegistry'],
-	'@woocommerce/settings'       : ['wc', 'wcSettings']
+  "@woocommerce/blocks-registry": ["wc", "wcBlocksRegistry"],
+  "@woocommerce/settings": ["wc", "wcSettings"],
 };
 
 const wcHandleMap = {
-	'@woocommerce/blocks-registry': 'wc-blocks-registry',
-	'@woocommerce/settings'       : 'wc-settings'
+  "@woocommerce/blocks-registry": "wc-blocks-registry",
+  "@woocommerce/settings": "wc-settings",
 };
 
 const requestToExternal = (request) => {
-	if (wcDepMap[request]) {
-		return wcDepMap[request];
-	}
+  if (wcDepMap[request]) {
+    return wcDepMap[request];
+  }
 };
 
 const requestToHandle = (request) => {
-	if (wcHandleMap[request]) {
-		return wcHandleMap[request];
-	}
+  if (wcHandleMap[request]) {
+    return wcHandleMap[request];
+  }
 };
 
 // Export configuration.
 module.exports = {
-	...defaultConfig,
-	entry: {
-		'frontend/blocks': '/src/PaymentMethod.jsx',
-	},
-	output: {
-		path: path.resolve( __dirname, 'assets/blocks' ),
-		filename: '[name].js',
-	},
-	plugins: [
-		...defaultConfig.plugins.filter(
-			(plugin) =>
-				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
-		),
-		new WooCommerceDependencyExtractionWebpackPlugin({
-			requestToExternal,
-			requestToHandle
-		})
-	]
+  ...defaultConfig,
+  entry: {
+    "frontend/payment-method": "/src/PaymentMethod.jsx",
+    "frontend/transparent": "/src/Transparent.jsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "assets/blocks"),
+    filename: "[name].js",
+  },
+  externals: {
+    "@woocommerce/blocks-registry": ["wc", "wcBlocksRegistry"],
+    "@woocommerce/settings": ["wc", "wcSettings"],
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  plugins: [
+    ...defaultConfig.plugins.filter(
+      (plugin) =>
+        plugin.constructor.name !== "DependencyExtractionWebpackPlugin",
+    ),
+    new WooCommerceDependencyExtractionWebpackPlugin({
+      requestToExternal,
+      requestToHandle,
+    }),
+  ],
 };
