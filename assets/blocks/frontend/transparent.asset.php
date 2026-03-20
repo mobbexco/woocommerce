b5e39300c9b66d88674de7b0c8eeda71728570ba
mobbexLuis <luis.scarrone@mobbex.com>
2026-03-20 11:57:52 -0300

    update assets

--- assets/blocks/frontend/transparent.asset.php
+++ assets/blocks/frontend/transparent.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-i18n'), 'version' => 'a1cae952397d3e418a85');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-i18n'), 'version' => '9cf2682c3a308b40f83f');
